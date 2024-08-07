a197f8b60a4b1b489fcd7e81da198bf99d2712c1
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-07 14:50:45 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TryCatchInteringMore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       try
+        {
+            List<char> characters = new List<char>();
+            characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+            int value = characters[6];
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
